2bcc9b3c3b17bf73cd84c785c8977ee11b2f672b
matcha12345 <165014253+matcha12345@users.noreply.github.com>
2025-02-06 11:52:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0454e52560c7f5c55"
+    image_id           = "ami-04681163a08179f28"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
+    key_name = "bank-key"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
+     subnets = ["subnet-07a565e6d2837fc2f", "subnet-0b4b8ce1146a23b71"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
