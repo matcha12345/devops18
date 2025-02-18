96e29043a92990a379a446a68628c4c2605d49c4
matcha12345 <165014253+matcha12345@users.noreply.github.com>
2025-02-18 12:27:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-07a565e6d2837fc2f", "subnet-0b4b8ce1146a23b71"]
+     subnets = ["subnet-07a565e6d2837fc2f","subnet-0b4b8ce1146a23b71"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
