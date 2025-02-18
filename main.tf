5f992a8205dbec292184d50f038917210457fae3
matcha12345 <165014253+matcha12345@users.noreply.github.com>
2025-02-18 12:20:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-04681163a08179f28"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "bank-key"
+    key_name = "vpc-key"
     tags = {
         Name = "DevOps"
     }
