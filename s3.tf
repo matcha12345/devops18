cae51fe487cf9688faa9b708072fdb3e906e4199
matcha12345 <165014253+matcha12345@users.noreply.github.com>
2025-03-18 23:39:57 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "raju.k8s.flm"
+  bucket = "lokesh.k8s.flm"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -26,7 +26,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "raju.k8s.flm"
+bucket = "lokesh.k8s.flm"
 key = "prod/terraform.tfstate"
 }
 }
